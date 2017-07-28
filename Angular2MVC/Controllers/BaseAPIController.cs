c263fb2d22b5c9d0f6d9aa4ec76e21efc817471e
lasthero <chihho.chou@gmail.com>
2017-07-28 12:04:15 -0400

    Initial commit
    Initial commit for Angular2MVC

---
+++ Angular2MVC/Controllers/BaseAPIController.cs
@@ -0,0 +1,23 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Web.Http;
+
+namespace Angular2MVC.Controllers
+{
+    public class BaseAPIController : ApiController
+    {
+
+        protected readonly DBContext.Entities UserDB = new DBContext.Entities();
+        protected HttpResponseMessage ToJson(dynamic obj)
+        {
+            var response = Request.CreateResponse(HttpStatusCode.OK);
+            response.Content = new StringContent(JsonConvert.SerializeObject(obj), Encoding.UTF8, "application/json");
+            return response;
+        }
+    }
+}
---
+++ Angular2MVC/app/Components/home.component.ts
@@ -0,0 +1,8 @@
+﻿import {Component} from '@angular/core';
+
+//component metadata
+@Component({
+    template: `<img src="../../images/users.png" style="text-align:center"/>`
+})
+
+export class HomeComponent { }
